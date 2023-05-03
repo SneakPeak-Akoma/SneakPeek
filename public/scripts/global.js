b2964866e5ecb748971cdc6b1b058df54a68f384
sodiqlab <henryymustapha@gmail.com>
2023-05-03 14:19:51 -0400

    edits to the front end aspect of the project

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -59,11 +59,23 @@ const logOutHandler = async () => {
 
 // Nav Helper
 const setNav = (hasLoggedInUser) => {
-  const loggedOutNavHtml = `<ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="./create.html">Sign Up</a></li>
-    <li><a href="./login.html">Login</a></li>
-  </ul>`;
+  const loggedOutNavHtml = ` <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+  <div class="container px-4 px-lg-5">
+      <a class="navbar-brand" href="#page-top">SneakPeek</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ms-auto">
+              <li class="nav-item"><a class="nav-link" href="#about">Sign Up</a></li>
+              <li class="nav-item"><a class="nav-link" href="#projects">Log In</a></li>
+              <li class="nav-item"><a class="nav-link" href="#signup">WishList</a></li>
+              <li class="nav-item"><a class="nav-link" href="./user.html ">Profile</a></li>
+          </ul>
+      </div>
+  </div>
+</nav>`
 
   const loggedInNavHtml = `<ul>
     <li><a href="/">Home</a></li>
